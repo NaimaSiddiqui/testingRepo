ff3f34196cbe2902dbd0fd0dbd0d477d9ec2c438
NaimaSiddiqui <naima.siddiqui141@gmail.com>
2020-11-24 08:19:59 +0530

    Added platform, Naima

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1, platform;
 var backgroundImg;
 
 function preload() {
@@ -15,6 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+    platform =new Ground(150,305,300,170);
     
     ground = new Ground(600,height,1200,20)
 
@@ -59,4 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
